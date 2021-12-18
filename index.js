715a715e981bbe6aa3ade268c89fbbb2c8d4f87c
Shoh4g <80630421+Shoh4g@users.noreply.github.com>
2021-12-18 19:29:11 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+var randomNumber1 = Math.floor(Math.random()*6)+1;
+
+var img1 = "images/dice" + randomNumber1 + ".png";
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", img1);
+
+
+var randomNumber2 = Math.floor(Math.random()*6)+1;
+
+var img2 = "images/dice" + randomNumber2 + ".png";
+
+var image2 = document.querySelectorAll("img")[1];
+
+image2.setAttribute("src", img2);
+
+
+if (randomNumber1 > randomNumber2)
+{
+    document.getElementsByTagName('h1')[0].innerHTML = "Player 1 Wins!";
+}
+
+else if (randomNumber1 < randomNumber2)
+{
+    document.getElementsByTagName('h1')[0].innerHTML = "Player 2 Wins!";
+}
+
+else{
+    document.getElementsByTagName('h1')[0].innerHTML = "Draw!";
+}
+
